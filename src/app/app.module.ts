cdfa6f0f2f45491f6fb0e365f87b6fac365de377
Victor Toschi <victor.toschi@hotmail.com>
2017-12-27 19:35:09 -0200

    app.component update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 import { AsideListComponent } from './aside-list/aside-list.component';
+import { SearchComponent } from './shared/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AsideListComponent
+    AsideListComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
