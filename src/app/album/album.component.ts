1e66b4dd60004b9c2afdbe28420bcc5983c45713
Victor Toschi <victor.toschi@hotmail.com>
2017-12-28 17:42:11 -0200

    Album component structures

--- src/app/album/album.component.ts
+++ src/app/album/album.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'album',
@@ -6,12 +6,13 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./album.component.scss']
 })
 
-export class AlbumComponent implements OnInit {
+export class AlbumComponent {
   @Input() userId = '';
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges() {
+
   }
 
 }
