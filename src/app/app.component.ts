1dc2a78abe4ec2bffa270e2971d1429e35841f48
Victor Toschi <victor.toschi@hotmail.com>
2017-12-28 13:15:37 -0200

    Album component created and receiving the user id from parent component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,9 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
+  userId: string;
 
-  onUserIdSelected(event) { }
+  onUserIdSelected(event) {
+    this.userId = event;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { SharedModule } from './shared/shared.module';
 
+import { AlbumComponent } from './album/album.component';
 import { AppComponent } from './app.component';
 import { AsideListComponent } from './aside-list/aside-list.component';
 import { SearchComponent } from './shared/search/search.component';
@@ -11,6 +12,7 @@ import { SearchComponent } from './shared/search/search.component';
   declarations: [
     AppComponent,
     AsideListComponent,
+    AlbumComponent,
   ],
   imports: [
     BrowserModule,
