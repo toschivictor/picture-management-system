3579308c30e8037cb7e9bef678e93e81cb4da767
Victor Toschi <victor.toschi@hotmail.com>
2017-12-27 23:02:30 -0200

    Search component adjusments

--- src/app/shared/search/search.component.ts
+++ src/app/shared/search/search.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.scss']
 })
 
 export class SearchComponent {
