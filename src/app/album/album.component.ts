fe0c009b7967fb049556da02d55d8e36bf121fc9
Victor Toschi <victor.toschi@hotmail.com>
2017-12-29 12:33:48 -0200

    Eventemitter to parent component

--- src/app/album/album.component.ts
+++ src/app/album/album.component.ts
@@ -1,15 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { AlbumService } from './album.service';
 
 @Component({
   selector: 'album',
   templateUrl: './album.component.html',
   styleUrls: ['./album.component.scss'],
-  providers: [ AlbumService ]
+  providers: [AlbumService]
 })
 
 export class AlbumComponent {
   @Input() userId = '';
+  @Output() selectedAlbums = new EventEmitter();
+
   albums: Object;
   selectAllAlbums = false;
 
@@ -24,10 +26,29 @@ export class AlbumComponent {
       response => this.albums = response,
       error => console.log(error)
     );
+
+    this.selectAllAlbums = false;
   }
 
-  onSelectAll(event) {
-    this.selectAllAlbums = event;
+  onSelectAllAlbums(selected) {
+    this.selectAllAlbums = selected;
+
+    if (selected) {
+      const ids = [];
+
+      for (const prop in this.albums) {
+        ids.push({ albumId: this.albums[prop].id });
+      }
+
+      return this.selectedAlbums.emit(ids);
+    }
+
+  }
+
+  onSelectAlbum(isChecked, albumId) {
+    if (isChecked) {
+      return this.selectedAlbums.emit([{ albumId: albumId }]);
+    }
   }
 
 }
